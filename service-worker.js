ea0656118a45e4badca49e532d76bdf3f2740d93
manoaverner-maker <manoa.verner@gmail.com>
2026-07-05 13:55:04 +0200

    Desktop: App ab 1100px Breite 10% groesser darstellen (zoom)

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
    damit der Browser die neuen Dateien laedt statt der alten.
    ===================================================================== */
 
-const CACHE_NAME = 'sparkurs-v13';
+const CACHE_NAME = 'sparkurs-v14';
 
 /* Alle Dateien, die fuer den Offline-Betrieb gebraucht werden.
    Pfade relativ ("./"), damit es auch in einem Unterordner (GitHub Pages)
